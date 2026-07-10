caef2a7c7c3ad86d162722a941650740021c9b44
oskman <42803141+oskman@users.noreply.github.com>
2026-07-10 16:06:14 +0200

    React setup for dashboard, sample backend logic for dashboard

---
+++ dashboard/backend/api-gateway/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Status: API gateway is online"}
+
+@app.get("/api/devices")
+def get_devices():
+    return {"List of devices goes here"}
